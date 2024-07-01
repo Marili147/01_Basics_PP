d2d479aa6871d4c91a9543028b75d6072ea8fac5
Marili147 <simonegrossmann7@gmail.com>
2024-07-01 10:50:35 +0000

    02 / 03 test

--- src/02_variablen.js
+++ src/02_variablen.js
@@ -15,10 +15,35 @@
 
 /* Deklaration + Wertzuweisung I */
 
-let firstName; //Deklaration (Definition)
-console.log(firstName); // Ausgabe
-firstName = "Max"; // Wertzuweisung assignment
+//let firstName; //Deklaration (Definition)
+//console.log(firstName); // Ausgabe
+//firstName = "Max"; // Wertzuweisung assignment
 
-console.log(firstName); // Ausgabe
-let familyName = "Mütze"; // * Deklaration + Wertzuweisung I */
-console.log("Hi, ich bin " + firstName +" " + familyName + "!"); //Ausgabe  -- Hi, ich bin Max Mütze
+//console.log(firstName); // Ausgabe
+//let familyName = "Mütze"; // * Deklaration + Wertzuweisung I */
+//console.log("Hi, ich bin " + firstName +" " + familyName + "!"); //Ausgabe  -- Hi, ich bin Max Mütze
+
+let test;
+//test = "hi";
+//test = 2;
+
+
+
+console.log("Datentyp:" + typeof test); // Ausgabe Datentyp
+console.log("Inhalt:" + test); // Ausgabe Inhalt
+console.log("----------------")
+
+test = "hi"; // strings
+console.log("Datentyp:" + typeof test); // Ausgabe Datentyp
+console.log("Inhalt:" + test); // Ausgabe Inhalt
+console.log("----------------")
+
+test =2; // number
+console.log("Datentyp:" + typeof test); // Ausgabe Datentyp
+console.log("Inhalt:" + test); // Ausgabe Inhalt
+console.log("----------------")
+
+test = true
+console.log("Datentyp:" + typeof test); // Ausgabe Datentyp
+console.log("Inhalt:" + test); // Ausgabe Inhalt
+console.log("----------------")
