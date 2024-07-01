8012b0dab516cdb673e081760f01d975e1b6cb43
Marili147 <simonegrossmann7@gmail.com>
2024-07-01 10:25:34 +0000

    02 / 02 Deklaration und Wertzuweisung

--- src/02_variablen.js
+++ src/02_variablen.js
@@ -1,4 +1,4 @@
-console.log("hi");
+// console.log("hi");
 
 /**
  * 
@@ -7,3 +7,18 @@ console.log("hi");
  * 
  * 
  */
+// console.log("hi");
+// 
+
+// console.log(hi);
+
+
+/* Deklaration + Wertzuweisung I */
+
+let firstName; //Deklaration (Definition)
+console.log(firstName); // Ausgabe
+firstName = "Max"; // Wertzuweisung assignment
+
+console.log(firstName); // Ausgabe
+let familyName = "Mütze"; // * Deklaration + Wertzuweisung I */
+console.log("Hi, ich bin " + firstName +" " + familyName + "!"); //Ausgabe  -- Hi, ich bin Max Mütze
